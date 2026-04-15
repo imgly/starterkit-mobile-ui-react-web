44bdc8f798e858268bc4c56a3bf71c307ac51ce5
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-04-15 10:36:47 +0200

    Update starterkit for v1.73.0-rc.3

---
+++ src/app/components/AddTextSecondary/AddTextSecondary.jsx
@@ -0,0 +1,32 @@
+import { useEditor } from '../../contexts/EditorContext';
+import { autoPlaceBlockOnPage } from '../../../imgly/CreativeEngineUtils';
+import FontSelect from '../FontSelect/FontSelect';
+import SlideUpPanel, {
+  SlideUpPanelBody,
+  SlideUpPanelHeader
+} from '../SlideUpPanel/SlideUpPanel';
+
+const AddTextSecondary = ({ onClose }) => {
+  const { engine, currentPageBlockId } = useEditor();
+
+  const addText = (font, typeface) => {
+    const block = engine.block.create('text');
+    engine.block.setFont(block, font.uri, typeface);
+    engine.block.setFloat(block, 'text/fontSize', 40);
+    engine.block.setEnum(block, 'text/horizontalAlignment', 'Center');
+    engine.block.setHeightMode(block, 'Auto');
+    const pageWidth = engine.block.getWidth(currentPageBlockId);
+    engine.block.setWidth(block, pageWidth * 0.5);
+    autoPlaceBlockOnPage(engine, currentPageBlockId, block);
+  };
+
+  return (
+    <SlideUpPanel isExpanded onExpandedChanged={(value) => !value && onClose()}>
+      <SlideUpPanelHeader headline="Add Text"></SlideUpPanelHeader>
+      <SlideUpPanelBody>
+        <FontSelect onSelect={(font, typeface) => addText(font, typeface)} />
+      </SlideUpPanelBody>
+    </SlideUpPanel>
+  );
+};
+export default AddTextSecondary;
