8a72933de6617729ed67a0cb516083021b76493a
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-05-09 21:48:33 +0200

    Update starterkit for v1.75.0-rc.5

--- src/app/components/Slider/Slider.tsx
+++ src/app/components/Slider/Slider.tsx
@@ -1,33 +1,36 @@
-import type { ComponentProps } from 'react';
 import classes from './Slider.module.css';
 import classNames from 'classnames';
 
-import ReactSlider from 'react-slider';
+import ReactSlider, { ReactSliderProps } from 'react-slider';
 
-type SliderProps = ComponentProps<typeof ReactSlider>;
+type SliderProps = ReactSliderProps<number> & {
+  trackStartValue?: number;
+};
 
 const Slider = (props: SliderProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { trackStartValue, ...rest } = props;
   return (
-    <ReactSlider
+    <ReactSlider<number>
       className={classes.slider}
       thumbClassName={classes.thumb}
       trackClassName={classes.track}
-      renderThumb={({ key, ...props }) => (
-        <div {...props} key={key}>
+      renderThumb={({ key, ...thumbProps }) => (
+        <div {...thumbProps} key={key}>
           <span className={classes.innerThumb} />
         </div>
       )}
-      renderTrack={({ key, ...props }, state) => (
+      renderTrack={({ key, ...trackProps }, state) => (
         <div
-          {...props}
+          {...trackProps}
           key={key}
           className={classNames(
-            props.className,
+            trackProps.className,
             classes[`track--part-${state.index}`]
           )}
         />
       )}
-      {...props}
+      {...rest}
     />
   );
 };
